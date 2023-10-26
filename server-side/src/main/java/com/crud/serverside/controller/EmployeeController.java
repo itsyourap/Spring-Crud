2ffd2567af2d6245af3897752922877b14d33a0c
bishakhne0gi <bneogi102002@gmail.com>
2023-10-26 13:12:49 +0530

    feat: client-side

--- server-side/src/main/java/com/crud/serverside/controller/EmployeeController.java
+++ server-side/src/main/java/com/crud/serverside/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.crud.serverside.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.crud.serverside.repository.EmployeeRepository;
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 
+    @Autowired
     private EmployeeRepository employeeRepository;
 
     @GetMapping("/employees")
