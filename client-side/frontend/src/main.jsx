b735700438ec91095b215939e9129ab0bd0d9c06
Ankan Pal <itsyourap@gmail.com>
2024-01-12 10:04:34 +0530

    Update React Router

--- client-side/frontend/src/main.jsx
+++ client-side/frontend/src/main.jsx
@@ -4,10 +4,9 @@ import './App.css'
 
 
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter,
+    RouterProvider,
 } from "react-router-dom";
-
 
 
 import Landing from './component/landing/Landing';
@@ -17,42 +16,56 @@ import UpdateEmployee from './component/updateEmployee/UpdateEmployee';
 import AddTask from "./component/addTask/AddTask.jsx";
 import UpdateTask from "./component/updateTask/UpdateTask.jsx";
 import TaskList from "./component/taskList/TaskList.jsx";
-
+import AttendanceList from "./component/attendance/attendanceList/AttendanceList.jsx";
+import AddAttendance from "./component/attendance/addAttendance/AddAttendance.jsx";
+import UpdateAttendance from "./component/attendance/updateAttendance/UpdateAttendance.jsx";
 
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Landing />,
-  },
-  {
-    path: "/employeeList",
-    element: <EmployeeList />,
-  },
-  {
-    path: "employeeList/addEmployee",
-    element: <AddEmployee />,
-  },
-  {
-    path: "employeeList/updateEmployee/:id",
-    element: <UpdateEmployee />,
-  },
-  {
-    path: "/taskList",
-    element: <TaskList />
-  },
-  {
-    path: "taskList/addTask",
-    element: <AddTask />
-  },
-  {
-    path: "taskList/updateTask/:id",
-    element: <UpdateTask />
-  }
+    {
+        path: "/",
+        element: <Landing/>,
+    },
+    {
+        path: "/employeeList",
+        element: <EmployeeList/>,
+    },
+    {
+        path: "employeeList/addEmployee",
+        element: <AddEmployee/>,
+    },
+    {
+        path: "employeeList/updateEmployee/:id",
+        element: <UpdateEmployee/>,
+    },
+    {
+        path: "/taskList",
+        element: <TaskList/>
+    },
+    {
+        path: "taskList/addTask",
+        element: <AddTask/>
+    },
+    {
+        path: "taskList/updateTask/:id",
+        element: <UpdateTask/>
+    },
+    {
+        path: "/attendanceList",
+        element: <AttendanceList/>
+    },
+    {
+        path: "attendanceList/addAttendance",
+        element: <AddAttendance/>
+    },
+    {
+        path: "attendanceList/updateAttendance/:id",
+        element: <UpdateAttendance/>
+    }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
