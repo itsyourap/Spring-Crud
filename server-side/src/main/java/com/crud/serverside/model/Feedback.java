c27dae096fce96759f187e81b98dec681a840d32
Ankan Pal <itsyourap@gmail.com>
2024-01-20 00:05:44 +0530

    Add Feedback Model

---
+++ server-side/src/main/java/com/crud/serverside/model/Feedback.java
@@ -0,0 +1,48 @@
+package com.crud.serverside.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+
+@Table(name = "feedback")
+
+public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    private Employee employee;
+
+    @Column(name = "feedback")
+    private String feedback;
+
+    public Feedback() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
