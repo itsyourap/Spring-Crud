f3e54c7f92d774960d0ef10e74659122b96037f5
Ankan Pal <itsyourap@gmail.com>
2024-01-19 23:48:55 +0530

    Implement Notification Controller

---
+++ server-side/src/main/java/com/crud/serverside/controller/NotificationController.java
@@ -0,0 +1,65 @@
+package com.crud.serverside.controller;
+
+import com.crud.serverside.exception.ResourceNotFoundException;
+import com.crud.serverside.model.Notification;
+import com.crud.serverside.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/v1/")
+public class NotificationController {
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    // Get All Notifications
+    @GetMapping("/notifications")
+    public List<Notification> getAllNotifications() {
+        return notificationRepository.findAll();
+    }
+
+    // Create New Notification
+    @PostMapping("/notifications")
+    public Notification createNotification(@RequestBody Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    // Get Notification by ID
+    @GetMapping("/notifications/{id}")
+    public ResponseEntity<Notification> getNotificationById(@PathVariable Long id) {
+        Notification notification = notificationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Notification does not exist with id:" + id));
+
+        return ResponseEntity.ok(notification);
+    }
+
+    @PutMapping("/notifications/{id}")
+    public ResponseEntity<Notification> updateNotification(@PathVariable Long id, @RequestBody Notification notificationDetails) {
+        Notification notification = notificationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Notification does not exist with id:" + id));
+
+        notification.setNotificationTitle(notificationDetails.getNotificationTitle());
+        notification.setNotificationContent(notificationDetails.getNotificationContent());
+
+        Notification updateNotification = notificationRepository.save(notification);
+        return ResponseEntity.ok(updateNotification);
+    }
+
+    // Delete Notification
+    @DeleteMapping("/notifications/{id}")
+    public Map<String, Boolean> deleteNotification(@PathVariable Long id) {
+        Notification notification = notificationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Notification does not exist with id:" + id));
+        notificationRepository.delete(notification);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Deleted", Boolean.TRUE);
+        return response;
+    }
+}
