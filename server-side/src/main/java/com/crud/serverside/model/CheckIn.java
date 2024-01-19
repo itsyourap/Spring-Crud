25f2122e70b1da91d5a92f0bd2b091fcc947ea8b
Ankan Pal <itsyourap@gmail.com>
2024-01-19 22:16:52 +0530

    Create Check In model

---
+++ server-side/src/main/java/com/crud/serverside/model/CheckIn.java
@@ -0,0 +1,54 @@
+package com.crud.serverside.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "checkins")
+public class CheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    private Employee employee;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    public CheckIn(){
+
+    }
+
+    public CheckIn(long id, Employee employee, LocalDateTime dateTime) {
+        this.id = id;
+        this.employee = employee;
+        this.dateTime = dateTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
